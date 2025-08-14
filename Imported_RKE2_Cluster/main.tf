7ddc9fb75f7b5159992c7832fb36006afd00f646
Durga-Sirisha-Karuturi <84554281+Durga-Sirisha-Karuturi@users.noreply.github.com>
2025-08-14 12:12:52 +0530

    Update main.tf

--- Imported_RKE2_Cluster/main.tf
+++ Imported_RKE2_Cluster/main.tf
@@ -1,12 +1,60 @@
-cluster_name  = "custom-cluster"
-rancher_token = "token-gbt44:nxr4v5nb5sh9sgwc225rjbnpzl2x4984bzxnls495d66xtbflx46v9"
-rancher_url   = "https://100.64.188.230.sslip.io"
-ssh_password  = "p@SSW0RD@1234"
-ssh_user      = "sifyadm"
-
-vms = [
-  {
-    ip    = "100.64.188.228"
-    roles = ["controlplane", "etcd", "worker"]
+terraform {
+  required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    rancher2 = {
+      source  = "rancher/rancher2"
+    }
   }
-]
+}
+
+variable "rancher_url" {}
+variable "rancher_token" {
+  sensitive = true
+}
+variable "cluster_name" {}
+variable "ssh_user" {}
+variable "ssh_password" {
+  sensitive = true
+}
+variable "vms" {
+}
+
+provider "rancher2" {
+  api_url   = var.rancher_url
+  token_key = var.rancher_token
+  insecure  = true # set to false if using valid certs
+}
+
+resource "rancher2_cluster" "custom_cluster" {
+  name        = "${var.cluster_name}"
+}
+
+
+locals {
+  base_command = rancher2_cluster.custom_cluster.cluster_registration_token.0.insecure_command
+}
+
+# 2. SSH into each VM and run the command
+resource "null_resource" "register_nodes" {
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo '${var.ssh_password}' | sudo -S sh -c '${replace(local.base_command, "'", "'\"'\"'")}'"
+    ]
+
+    connection {
+      type     = "ssh"
+      host     = var.vms
+      user     = var.ssh_user
+      password = var.ssh_password
+    }
+  }
+}
+
+output "base_registration_command" {
+  value     = local.base_command
+  sensitive = false
+}
